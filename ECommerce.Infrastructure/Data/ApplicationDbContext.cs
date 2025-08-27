547a1c2ec122c9452b49b260d8958a5f62a14b64
Nabin Kandel <nabin.kandel@oic.edu.np>
2025-08-27 20:56:19 +0545

    Update DB Connection

--- ECommerce.Infrastructure/Data/ApplicationDbContext.cs
+++ ECommerce.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,14 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ECommerce.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Infrastructure.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        // Define DbSets for your entities here
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category);
 
+            modelBuilder.Entity<Category>();
+
+        }
     }
 }
