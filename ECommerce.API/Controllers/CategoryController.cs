b8ee0d3cd72419a6a24487d30aef738a6cfeb1c4
Nabin Kandel <nabin.kandel@oic.edu.np>
2025-09-04 08:26:38 +0545

    Update category control with action method get and post

---
+++ ECommerce.API/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using ECommerce.Application.DTOs;
+using ECommerce.Application.Interfaces;
+using ECommerce.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(
+            ICategoryRepository categoryRepository,
+             IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            var result = _mapper.Map<IEnumerable<CategoryDTO>>(categories);
+            return Ok(result);
+        }
+        [HttpPost("Category")]
+        public async Task<IActionResult> CreateCategory([FromBody] CategoryDTO categoryDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var category = _mapper.Map<Category>(categoryDto);
+            var createdCategory = await _categoryRepository.AddAsync(category);
+            return Ok(_mapper.Map<CategoryDTO>(createdCategory));
+            
+        }
+    }
+}
